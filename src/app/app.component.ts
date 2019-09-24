90a9114f2f5a63a6cecafbcda794b68ed27aa431
Subhajitdas298 <Subhajitdas298@users.noreply.github.com>
2019-09-25 00:34:35 +0530

    Added slideout panel system

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, ElementRef } from '@angular/core';
+import * as Slideout from 'node_modules/slideout';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'slide';
+export class AppComponent implements OnInit {
+
+  slideout: Slideout;
+
+  constructor(private elRef:ElementRef) {}
+
+  ngOnInit(): void {
+    this.slideout = new Slideout({
+      'panel': this.elRef.nativeElement.querySelector('#panel'),
+      'menu': this.elRef.nativeElement.querySelector('#menu'),
+      'padding': 256,
+      'tolerance': 70
+    });
+  }
+
+  toggle() {
+    this.slideout.toggle();
+  }
 }
